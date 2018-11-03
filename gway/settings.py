7a5c81e9ee9c2a42e40d6e3100cf8e6ad5adf6b6
Chaz Selph <chaz@chaz.local>
2018-11-02 21:25:39 -0600

    initialize postgresql, delete sqlite

--- gway/settings.py
+++ gway/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'gway.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gway',
+        'USER': 'postgres',
+        'PASSWORD': '123god1;',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
