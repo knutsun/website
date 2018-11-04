b581e553c5ab2aa4100ff8b391cb6345700c4108
Chaz Selph <chaz@chaz.local>
2018-11-03 18:07:49 -0600

    uncomment static_root

--- gway/settings.py
+++ gway/settings.py
@@ -157,10 +157,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, 'static/'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = (
+#   os.path.join(BASE_DIR, 'static/'),
+# )
 
 LOGIN_REDIRECT_URL = 'home' #alternatively, can use '/'
 LOGOUT_REDIRECT_URL = 'home'
