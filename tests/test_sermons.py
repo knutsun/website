758d0f5ffb8ed42690635030eed93f43b13f5933
Chaz Selph <csselph@gmail.com>
2019-07-07 01:42:28 -0600

    update

--- tests/test_sermons.py
+++ tests/test_sermons.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.core.files import File
 from django.db import models
+from sermons.models import Sermons
 
 from unittest.mock import MagicMock
 import datetime
@@ -12,17 +13,22 @@ class TestHome(TestCase):
 
     def test_sermon_page_template(self):
         response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
 
     def test_can_create_sermon(self):
-
+        description = 'Sunday\'s Sermon'
         data = {
             'title': 'Test',
             'date': datetime.date(2019, 2, 1),
-            'description': 'Description',
+            'description': description,
             'file': MagicMock(spec=File)
 
         }
-        response = self.client.post('/api/sermons/', data=data)
-        self.assertEqual(response.status_code, 201)
+        post_response = self.client.post('/api/sermons/', data=data)
+        self.assertEqual(post_response.status_code, 201)
+        self.assertIn(description, post_response.content.decode())
+
+        sermons_response = self.client.get('/api/sermons/')
+        self.assertContains(sermons_response, description)
 
