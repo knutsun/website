0503e13d710bde29ebb52e4398a3979bfd372f27
Chaz Selph <csselph@gmail.com>
2019-07-07 00:45:55 -0600

    update test file

--- tests/test_sermons.py
+++ tests/test_sermons.py
@@ -1,8 +1,28 @@
 from django.test import TestCase
+from django.core.files import File
+from django.db import models
+
+from unittest.mock import MagicMock
+import datetime
+import six
+import pdb
 
 
 class TestHome(TestCase):
 
-    def test_sermons_page_template(self):
+    def test_sermon_page_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'index.html')
+
+    def test_can_create_sermon(self):
+
+        data = {
+            'title': 'Test',
+            'date': datetime.date(2019, 2, 1),
+            'description': 'Description',
+            'file': MagicMock(spec=File)
+
+        }
+        response = self.client.post('/api/sermons/', data=data)
+        self.assertEqual(response.status_code, 201)
+
