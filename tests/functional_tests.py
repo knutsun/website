bff85aabc33f6839b11a0df839eb8e059398c543
Chaz Selph <csselph@gmail.com>
2020-03-30 15:18:44 -0600

    Add functional test to verify Contact POST

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,11 +1,39 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+import time
 
-browser = webdriver.Firefox()
 
-# User opens website
-browser.get('http://localhost:8000')
+with webdriver.Firefox() as driver:
 
-# User reads Gateway Baptist Church in the web page title
-assert 'Gateway Baptist Church' in browser.title
+    wait = WebDriverWait(driver, 10)
+    driver.get("http://localhost:8000/contact")
 
-browser.quit()
+    navbar = driver.find_element_by_class_name('navbar')
+
+    subject = driver.find_element_by_name('subject')
+    name = driver.find_element_by_name('name')
+    email = driver.find_element_by_name('email')
+    message = driver.find_element_by_name('body')
+    submit_button = driver.find_element_by_id('submit')
+
+    subject.send_keys('Prayer Request')
+    time.sleep(1)
+
+    name.send_keys('Chaz')
+    time.sleep(1)
+
+    email.send_keys('csselph@gmail.com')
+    time.sleep(1)
+
+    message.send_keys('Please Lord, help our friend Brian during this trying time.')
+    time.sleep(1)
+
+    submit_button.click()
+
+    time.sleep(5)
+
+    alert_message = driver.find_element_by_xpath("//div[@class='alert alert-primary messages']")
+
+    time.sleep(2)
+    print("Test succeeded")
+    driver.quit()
