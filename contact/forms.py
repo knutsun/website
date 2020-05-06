8fe645621331e68c03b4ff5faa6818bf9cece577
Chaz Selph <csselph@gmail.com>
2020-05-05 21:24:21 -0600

    Back-end validation for contact

--- contact/forms.py
+++ contact/forms.py
@@ -1,4 +1,6 @@
 from django.forms import ModelForm
+from django.forms import ValidationError
+from django import forms
 
 from .models import Contact
 
@@ -14,3 +16,12 @@ class ContactForm(ModelForm):
             'email': {'required': 'Email is invalid. Please enter a valid email.'},
             'body': {'required': 'Message is invalid. Please enter a valid message.'}
         }
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+
+        if not email.endswith('@*.com'):
+            print('toperror')
+            raise ValidationError('Domain of email is not valid')
+
+        return email
